df6a41c0bddc24cd241bca6a7cf6d281e22542cd
saiypts <ypts729@gmail.com>
2024-05-17 09:52:54 +0530

    intial commit

---
+++ src/input.kt
@@ -0,0 +1,10 @@
+fun main(){
+    println("hello world");
+    var a=20
+    var b=30
+    var sum=a+b
+    println(sum)
+
+    var minValue: Float=Float.MIN_VALUE
+    println("min value ${minValue}")
+}
